113bdc01bdeba305ad827026e003823cfa5f5ab8
Joalzin <jojokamilanezi@gmail.com>
2026-05-19 22:27:42 -0300

    Add files via upload

---
+++ aula - 14/s1nb-script.js
@@ -0,0 +1,36 @@
+var issoEumaString = "isso é uma string";
+let issoEmNumero = 41;
+const issoEumBooleano = true;
+let issoTambemEumaString = "42";
+
+if (issoEumnumero === IssotambemEumaString) {
+    console.log("as Strings São Iguais")
+} else {
+    console.log("AS Strings São Diferentes"); 'As Strings São Diferentes'
+}
+
+
+console.log(issoEumNumero + IssoTambemEumaString) 
+
+console.log(issoEmNumero + issoEmNumero)
+
+var IssoEumaGlobal = "Eu Sou Uma Variavel Global"
+
+{
+    let issoEumaLocal = "Eu Sou Uma Variavel Local";
+    console.log (issoEumaGlobal);
+}
+
+console.log(issoEumaGlobal);  
+//cosole.log (issoEumaLocal); // Isso causará um erro, pois IssoEumaLocal, não está definido fora do bloco
+
+let floatvaluel1 = 0.06;
+let floatvaluel2 = 0.
+let sum = floatvalue1 + floatvalue2
+
+console.log(sum.toPrecision(2)); // Isso pode resultar em 0,07000000000001 devido a precisao dos numer ode ponte flutuante.
+
+for (let i = 0; i < 5; i++) {
+    console.log(i); 0, 1, 2, 3, 4
+    console.log(i*2); 0, 2, 4, 6, 8
+} 
